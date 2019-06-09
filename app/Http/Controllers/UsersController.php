71ed328a17b283cfb96b9e5df9339c1d693f27d9
yfs <yfssen@126.com>
2019-06-09 20:50:05 +0800

    分类模型和数据

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+
+    }
     public function show(User $user)
     {
 
@@ -15,14 +20,16 @@ class UsersController extends Controller
     }
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
        $data = $request->all();
        if($request->avatar){
-           $result = $uploader->save($request->avatar, 'avatars', $user->id);
+           $result = $uploader->save($request->avatar, 'avatars', $user->id,416);
            if ($result) {
                $data['avatar'] = $result['path'];
            }
