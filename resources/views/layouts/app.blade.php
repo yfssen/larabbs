ffa78f4e2e3dd969af2d66d3d57a2b029d6f21e7
yfs <yfssen@126.com>
2019-06-11 20:40:24 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS') - 论坛</title>
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
