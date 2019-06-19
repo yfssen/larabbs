297edfeb350ba0d8e29c250756f1f497d6e7c4a6
yfs <yfssen@126.com>
2019-06-19 04:11:28 +0000

    回复列表

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -9,7 +9,7 @@ class Reply extends Model
     //一条回复属于一个话题
     public function topic()
     {
-        return $this ->belongsTo(Category::class);
+        return $this ->belongsTo(Topic::class);
     }
    //一条回复属于一个作者所有
     public function user()
@@ -19,3 +19,20 @@ class Reply extends Model
     }
 
 }
+
+/*
+namespace App\Models;
+class Reply extends Model
+{
+    protected $fillable = ['content'];
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}*/
