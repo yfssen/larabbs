4ae92738d9e9bbd74ea45727ee7c0ec4a075b465
yfs <yfssen@126.com>
2019-06-19 17:20:49 +0800

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //上传路由
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+//消息通知
+Route::resource('notifications','NotificationsController',['only'=>['index']]);
