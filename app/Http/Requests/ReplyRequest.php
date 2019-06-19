af013a088f81dfa74aa8d812cb390f5c375a661d
yfs <yfssen@126.com>
2019-06-19 16:10:02 +0800

    用户可以发表评论

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -6,36 +6,41 @@ class ReplyRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
+
+        return [
+            'content' => 'required|min:2',
+        ];
+//        switch($this->method())
+//        {
+//            // CREATE
+//            case 'POST':
+//            {
+//                return [
+//                    // CREATE ROLES
+//                ];
+//            }
+//            // UPDATE
+//            case 'PUT':
+//            case 'PATCH':
+//            {
+//                return [
+//                    // UPDATE ROLES
+//                ];
+//            }
+//            case 'GET':
+//            case 'DELETE':
+//            default:
+//            {
+//                return [];
+//            };
+//        }
     }
 
     public function messages()
     {
         return [
             // Validation messages
+//            'content.min'=>'评论内容至少两个字符',
         ];
     }
 }
